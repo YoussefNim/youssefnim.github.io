3eed1bb0b6db52f5d7bd60358208b05e4594df38
YoussefNim <youssef.nimzilne@gmail.com>
2024-10-09 19:09:37 +0100

    removed contact form since no server side, showed error

--- myblog.js
+++ myblog.js
@@ -1,7 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 const toggle_mode_btn = document.getElementById("toggle_mode_btn")
-
 const storedTheme = localStorage.getItem('theme')
 
 // If a theme preference is stored, apply it to the body classList
